d14f8d50199c58307477d151103c3fc26fac6f67
Patrick Vatterott <patrick.vatterott@gmail.com>
2013-04-05 10:22:51 -0400

    Added multiple inline function test

---
+++ tests/t12.c
@@ -0,0 +1,26 @@
+int main() {
+  int a, b, c;
+  a = 3;
+  b = 4;
+  
+  c = foo(a, b) + 3;
+  //return b * bar(c, a); 
+}
+
+int foo(int a, int b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+int bar(int a, int b) {
+  int c;
+  c = 0;
+  while (a < b) {
+    a = a + 1;
+    c = c + a;
+  }
+  return c;
+}
